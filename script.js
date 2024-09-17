38872d1b7c78fc2a24a101d6be84a0c2d710f720
Edvard <140340191+EdvardsCorp@users.noreply.github.com>
2024-09-17 11:48:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -15,13 +15,14 @@ const SkillsText =
   "I mostly have experience in HTML, CSS and JavaScript." +
   "For hobbies, I have learned the following: Flutter & Dart, along with Google Firebase(Realtime Database, Firestore Database, Analytics, Authentication, Storage).";
 
-const PortfolioText = `
+const PortfolioText = 
       <p>The portfolio is currently being updated. Soon, I will add the projects I have been working on in my free time.</p>
       <blockquote class="imgur-embed-pub" lang="en" data-id="a/a09p58K" data-context="false">
         <a href="//imgur.com/a/a09p58K">View Portfolio</a>
       </blockquote>
       <script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>
-    `;
+<a href="https://ee.linkedin.com/in/edvard-prosutinski-531743179"><i class="bi bi-linkedin"></i></a>
+    ;
 
 const ContactText =
   "You can contact me from Git, LinkedIn or Facebook by clicking icon bellow. Also my Email is Edvard.Prosutinski@gmail.com";
