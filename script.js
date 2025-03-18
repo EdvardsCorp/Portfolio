c9b06b586886513e53413df8a8d23a9655fefd90
Edvard <140340191+EdvardsCorp@users.noreply.github.com>
2025-03-18 21:58:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const SkillsText =
 const PortfolioText = `
 <div>
 <div>
- <img src="https://imgur.com/a/w5qgSlf" alt="Portfolio Image" style="width: 600px; height: 205px;">
+ <img src="https://i.imgur.com/KxF05lP.png" alt="Portfolio Image" style="width: 600px; height: 205px;">
   </div>
 <div>
  <img src="https://i.imgur.com/e2LqbJ8.png" alt="Portfolio Image" style="width: 600px; height: 205px;">
